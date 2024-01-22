b4235211d5a77633d65c18995a8a75ea719f9052
Kristina <tinakyukova@gmail.com>
2024-01-22 16:42:02 +0300

    feat: реализация страницы string и заполнение circle

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import stylesString from "./string.module.css";
 
 export const StringComponent: React.FC = () => {
+  const [arrString, setArrString] = useState<string[]>([]);
+
+  const onClick = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const string = e.target.value;
+    const arrayOfStrings = string.split("");
+    setArrString(arrayOfStrings);
+  };
+
+  // console.log(arrString);
   return (
     <SolutionLayout title="Строка">
-     
+      <main className={stylesString.main}>
+        <form className={stylesString.form}>
+          <Input maxLength={11} isLimitText onChange={onClick} />
+          <Button text="Развернуть" />
+        </form>
+        <ul className={stylesString.list}>
+          {arrString?.map((item) => (
+            <li className={stylesString.circle}>
+              <Circle letter={item} />
+            </li>
+          ))}
+        </ul>
+      </main>
     </SolutionLayout>
   );
 };
